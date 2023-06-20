db0d59ac729d71ec8c8dc7b68685e075d0b044be
Bill Greatness <billgreatness@outlook.com>
2023-06-20 19:48:15 +0000

    Product Edit

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,12 +1,56 @@
-import { useState } from "react";
-import { addData, uploadFile } from "../functions/calls";
+import { useEffect, useState, useMemo } from "react";
+import {
+  addData,
+  get1Doc,
+  updateDocument,
+  uploadFile,
+} from "../functions/calls";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Feedback } from "../components";
+
+export const useQuery = () => {
+  const { search } = useLocation();
+
+  return useMemo(() => new URLSearchParams(search), [search]);
+};
+
 export default function AddProduct() {
+  const navigate = useNavigate();
+
   const [name, setName] = useState("");
   const [quantity, setQuantity] = useState(0);
   const [desc, setDescription] = useState("");
   const [photo, setPhoto] = useState("");
   const [wholeSalePrice, setWholeSalePrice] = useState("");
+  const [receiptPrice, setReceiptPrice] = useState("");
   const [retailPrice, setRetailPrice] = useState("");
+  const [open, setOpen] = useState(false)
+  const [message, setMessage] = useState("")
+  const [errType, setErrType] = useState("")
+
+  const query = useQuery();
+
+  const item_id = query.get("id");
+
+  useEffect(() => {
+    async function getProduct() {
+      if (item_id !== null) {
+        // setDefault Value
+        const item = await get1Doc({ path: "products", id: item_id });
+        if (item.exists()) {
+          const { name, desc, photo, quantity, price } = item.data();
+          setName(name);
+          setDescription(desc);
+          setPhoto(photo);
+          setQuantity(quantity);
+          setWholeSalePrice(price.wholeSalePrice);
+          setRetailPrice(price.retailPrice);
+          setReceiptPrice(price.receiptPrice);
+        }
+      }
+    }
+    getProduct();
+  }, [item_id]);
 
   const addProduct = (e) => {
     event.preventDefault();
@@ -16,34 +60,77 @@ export default function AddProduct() {
       quantity,
       desc,
       photo,
-      price:{
-        wholeSalePrice, 
-        retailPrice
+      price: {
+        wholeSalePrice,
+        retailPrice,
+        receiptPrice,
       },
-      dateAdded: new Date().toLocaleString(),
+      dateAdded: new Date().toLocaleString("ru"),
     };
 
-    addData({
-      path: "products",
-      data,
-      id: new Date().getTime().toString(),
-    })
-      .then(() => {
-        alert("Product Successfuly Added!")
-        setName("")
-        setDescription("")
-        setPhoto("")
-        setQuantity(0)
-        setWholeSalePrice(0)
-        setRetailPrice(0)
+    if (item_id !== null) {
+      // update Document
+      updateDocument({
+        path: "products",
+        id: item_id,
+        data: { ...data, dateUpdated: new Date().toLocaleString("ru") },
       })
-      .catch((err) => {
-        console.log(err);
-      });
+        .then(() => {
+          setOpen(true)
+          setMessage("Product Update Successful")
+          setErrType("success")
+
+
+          // reset form.
+          setName("");
+          setDescription("");
+          setPhoto("");
+          setQuantity(0);
+          setWholeSalePrice(0);
+          setRetailPrice(0);
+          setReceiptPrice(0)
+
+        })
+        .catch((err) => {
+          setOpen(true)
+          setMessage(err.message)
+          setErrType("error")
+        });
+    } else {
+      addData({
+        path: "products",
+        data,
+        id: new Date().getTime().toString(),
+      })
+        .then(() => {
+
+          setOpen(true)
+          setMessage("Product Addition Successful")
+          setErrType("success")
+
+          setName("");
+          setDescription("");
+          setPhoto("");
+          setQuantity(0);
+          setWholeSalePrice(0);
+          setRetailPrice(0);
+          setReceiptPrice(0)
+        })
+        .catch((err) => {
+          setOpen(true)
+          setMessage(err.message)
+          setErrType("error")
+        });
+    }
   };
 
   return (
-    <form autoComplete="off" onSubmit={addProduct} className="flex flex-col my-[55px] px-3">
+    <form
+      autoComplete="off"
+      onSubmit={addProduct}
+      className="flex flex-col my-[55px] px-3"
+    >
+      {open && <Feedback type={errType} message={message} close={setOpen} />}
       <div className="flex flex-col p-1 mb-2">
         <label htmlFor="product_name" className="text-xs pb-1">
           Product Name
@@ -69,7 +156,7 @@ export default function AddProduct() {
           {photo ? "Image Uploaded" : "Add Image"}
         </label>
         <input
-          required
+          required={photo === ""}
           className="hidden"
           onChange={(e) =>
             uploadFile({
@@ -89,6 +176,16 @@ export default function AddProduct() {
         </label>
         <div className="flex w-full">
           <input
+            value={receiptPrice}
+            required
+            onChange={(e) => setReceiptPrice(parseFloat(e.target.value))}
+            type="number"
+            step={0.1}
+            placeholder="Receipt Price"
+            id="product_price"
+            className="p-2 bg-white w-1/3 border border-gray-300 rounded-sm"
+          />
+          <input
             value={wholeSalePrice}
             required
             onChange={(e) => setWholeSalePrice(parseFloat(e.target.value))}
@@ -96,7 +193,7 @@ export default function AddProduct() {
             step={0.1}
             placeholder="Wholesale Price"
             id="product_price"
-            className="p-2 bg-white w-1/2 mr-1  border border-gray-300 rounded-sm"
+            className="p-2 bg-white w-1/3 mx-1  border border-gray-300 rounded-sm"
           />
           <input
             value={retailPrice}
@@ -106,7 +203,7 @@ export default function AddProduct() {
             step={0.1}
             placeholder="Retail  Price"
             id="product_price"
-            className="p-2 bg-white border w-1/2 border-gray-300 rounded-sm"
+            className="p-2 bg-white border w-1/3 border-gray-300 rounded-sm"
           />
         </div>
       </div>
@@ -142,7 +239,7 @@ export default function AddProduct() {
         type="submit"
         className="mx-auto bg-black text-white p-2 rounded-sm text-sm"
       >
-        Add Item
+        {item_id === null ? "Add Item" : "Update Item"}
       </button>
     </form>
   );
